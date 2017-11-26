9418d694a7708775a6e6e490b89c9b21f574f78e
hangboss1761 <1779043401@qq.com>
2017-11-26 22:31:24 +0800

    server

--- app/huxin/controller/LoginController.php
+++ app/huxin/controller/LoginController.php
@@ -61,12 +61,14 @@ class LoginController extends HomeBaseController
                     }
                  }
                 //验证手机号是否被注册
+
                 if(empty($data['passwordt'])){
                     $this->error("请输入密码！");
                 }elseif($data['password'] != $data['passwordt']){
                     $this->error("密码不一致！");
                 }else{
                     $cmsCode =  Db::name('hx_code')->where(array('phone' => $data['phone']))->field('code')->find();
+
                     if($data['cmsCode'] == $cmsCode['code']){
                         $res = array(
                             'phone'     => $data['phone'],
@@ -99,13 +101,43 @@ class LoginController extends HomeBaseController
 
 
     /**
-      *  发送验证码
+      *  发送验证码--注册
       */
+    public  function sendSMSl(){
+        $ch = curl_init();
+        // 必要参数
+        $apikey = "d0a614ab1e413bb0ef972f42d88fe57f"; //修改为您的apikey(https://www.yunpian.com)登录官网后获取
+
+        $mobile =  $_GET['mobile']; //请用手机号代替
+        $code = $this->createSMSCode();
+        $text="【天使家教】您的验证码是".$code."，如非本人操作，请忽略本短信";
+        //将验证码存入数据库
+        $data['phone'] = $mobile;
+        $data['code'] = $code;
+        $data['create_time'] = time();
+        $arrId = Db::name('hx_code')->where(array('phone'=>$data['phone']))->find();
+        $id = Db::name('hx_code')->where(array('id'=>$arrId['id']))->insert($data);
+        // 发送短信
+        $data=array('text'=>$text,'apikey'=>$apikey,'mobile'=>$mobile);
+        curl_setopt ($ch, CURLOPT_URL, 'https://sms.yunpian.com/v2/sms/single_send.json');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        $json_data = curl_exec($ch);
+        //解析返回结果（json格式字符串）
+        $array = json_decode($json_data,true);
+        $array['smsCode'] = $id;
+        echo json_encode($array);
+     }
+
+
+    /**
+     *  发送验证码
+     */
     public  function sendSMS(){
         $ch = curl_init();
         // 必要参数
         $apikey = "d0a614ab1e413bb0ef972f42d88fe57f"; //修改为您的apikey(https://www.yunpian.com)登录官网后获取
-        
+
         $mobile =  $_GET['mobile']; //请用手机号代替
         $code = $this->createSMSCode();
         $text="【天使家教】您的验证码是".$code."，如非本人操作，请忽略本短信";
@@ -125,8 +157,9 @@ class LoginController extends HomeBaseController
         $array = json_decode($json_data,true);
         $array['smsCode'] = $id;
         echo json_encode($array);
-        
-     }
+    }
+
+
      /**
       *  随机生成4位验证码
       */
