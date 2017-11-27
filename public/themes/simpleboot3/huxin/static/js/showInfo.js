b2fed5457cff354ccfedaa7a5c37c291805a0f42
hangboss1761 <1779043401@qq.com>
2017-11-27 14:02:39 +0800

    gyh

--- public/themes/simpleboot3/huxin/static/js/showInfo.js
+++ public/themes/simpleboot3/huxin/static/js/showInfo.js
@@ -47,5 +47,68 @@ $(document).ready(function () {
         });
         return false;
     });
+    function findIndex(str,times) {
+        var x = str.indexOf("/");
+        for (var i = 0; i < times; i++){
+            x = str.indexOf(str,x+1);
+        }
+        return x;
+    }
+    function getUrl(str,index) {
+        var url = str.substring(0,index-1);
+        return url;
+    }
+    function getPara(str,index) {
+        var para = str.substring(index+1);
+        para = para.split("/");
+        console.log("Para");
+        console.log(para);
+        return para;
+    }
+    $(".ajaxA").click(function () {
+        var str = $(".ajaxA").attr("href");
+        var index = findIndex(str,4);
+        var url = getUrl(str,index);
+        var para = getPara(str,index);
+        return false;
+        $.ajax({
+            type:"GET",
+            url:$(".ajaxA").attr("href"),
+            // data:decodeURIComponent($("form").serialize()),
+            dataType:"json",
+            success:function (data,textStatus) {
+                console.log(data);
+                if(data.code === 1){
+                    swal({
+                        title:data.msg,
+                        type:"success"
+                    });
+                }
+                else
+                {
+                    swal({
+                        title:data.msg,
+                        type:"error"
+                    });
+                }
+
+                //额外绑定一个事件，当确定执行之后返回成功的页面的确定按钮，点击之后刷新当前页面或者跳转其他页面
+                if (data.url !== ""){
+                    $('.confirm').click(function () {
+                        window.location = data.url;
+                    });
+                }
+                console.log(data);
+            },
+            error:function (error) {
+                swal({
+                    title:"error",
+                    type:"error"
+                });
+                console.log(error);
+            }
+        });
+        return false;
+    })
 })
 
