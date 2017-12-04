18d6941fc9d6f81fb767fc438e3024008e257d61
hangboss1761 <1779043401@qq.com>
2017-12-04 09:43:23 +0800

    gyh 一些验证规则

--- app/huxin/controller/UserController.php
+++ app/huxin/controller/UserController.php
@@ -208,9 +208,19 @@ class UserController extends CheckController
                     $this->error('请输入姓名！');
                 }elseif(empty($data['idcard'])){
                     $this->error('请输入身份证号码！');
-                }else{
-                    die;
-                    $this->redirect('user/idcard',['card'=>$data['idcard'], 'name'=>$data['name']]);
+                }elseif(!$this->validation_filter_id_card($data['idcard'])){
+                    $this->error('身份证号码格式错误！');
+                }
+                else{
+//                    $this->redirect('user/idcard',['card'=>$data['idcard'], 'name'=>$data['name']]);
+//                    $this->success('success',url('user/idcard',['card'=>$data['idcard'], 'name'=>$data['name']]));
+
+                    if ($this->idcard($data['idcard'],$data['name'])){
+                        $this->success("认证成功",url('user/grzx'));
+                    }
+                    else{
+                        $this->error("认证失败");
+                    }
                 }
             }
         }else{
@@ -219,6 +229,61 @@ class UserController extends CheckController
         return $this->fetch();
     }
 
+    // 计算身份证校验码，根据国家标准GB 11643-1999
+    function idcard_verify_number($idcard_base){
+        if(strlen($idcard_base)!=17){
+            return false;
+        }
+        //加权因子
+        $factor=array(7,9,10,5,8,4,2,1,6,3,7,9,10,5,8,4,2);
+        //校验码对应值
+        $verify_number_list=array('1','0','X','9','8','7','6','5','4','3','2');
+        $checksum=0;
+        for($i=0;$i<strlen($idcard_base);$i++){
+            $checksum += substr($idcard_base,$i,1) * $factor[$i];
+        }
+        $mod=$checksum % 11;
+        $verify_number=$verify_number_list[$mod];
+        return $verify_number;
+    }
+// 将15位身份证升级到18位
+    function idcard_15to18($idcard){
+        if(strlen($idcard)!=15){
+            return false;
+        }else{
+            // 如果身份证顺序码是996 997 998 999，这些是为百岁以上老人的特殊编码
+            if(array_search(substr($idcard,12,3),array('996','997','998','999')) !== false){
+                $idcard=substr($idcard,0,6).'18'.substr($idcard,6,9);
+            }else{
+                $idcard=substr($idcard,0,6).'19'.substr($idcard,6,9);
+            }
+        }
+        $idcard=$idcard.$this->idcard_verify_number($idcard);
+        return $idcard;
+    }
+// 18位身份证校验码有效性检查
+    function idcard_checksum18($idcard){
+        if(strlen($idcard)!=18){
+            return false;
+        }
+        $idcard_base=substr($idcard,0,17);
+        if($this->idcard_verify_number($idcard_base)!=strtoupper(substr($idcard,17,1))){
+            return false;
+        }else{
+            return true;
+        }
+    }
+//  身份证校验入口函数
+    function validation_filter_id_card($id_card){
+        if(strlen($id_card)==18){
+            return $this->idcard_checksum18($id_card);
+        }elseif((strlen($id_card)==15)){
+            $id_card=$this->idcard_15to18($id_card);
+            return $this->idcard_checksum18($id_card);
+        }else{
+            return false;
+        }
+    }
 
 /**
      * 实名认证API接口
@@ -263,10 +328,12 @@ class UserController extends CheckController
             $resp = (array)$obt['resp'];
             //名字转码
             $auname = urldecode($name);
-            return $this->redirect('user/checkidcard', ['resp'=>$resp['code'], 'idcard'=>$card, 'name'=>$auname, 'address'=>$data['address'], 'sex'=>$data['sex']]);
+            return $this->checkidcard($resp['code'],$card,$auname,$data['address'],$data['sex']);
+//                $this->redirect('user/checkidcard', ['resp'=>$resp['code'], 'idcard'=>$card, 'name'=>$auname, 'address'=>$data['address'], 'sex'=>$data['sex']]);
+
         }else{
-            $this->error('认证失败,请输入正确的身份证号码！');
-            // return false;
+//            $this->error('认证失败,请输入正确的身份证号码！');
+             return false;
         }
     }
   
@@ -289,9 +356,11 @@ class UserController extends CheckController
         );
         if($resp == 0){
             Db::name('hx_user')->where(['id' => $id])->update($data);
-            $this->success('认证成功', url('user/grzx'));
+//            $this->success('认证成功', url('user/grzx'));
+            return true;
         }else{
-            $this->error('认证失败');
+            return false;
+//            $this->error('认证失败');
         }
     }
     
